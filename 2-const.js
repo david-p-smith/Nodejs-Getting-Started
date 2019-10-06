625f71e87a6bec4961a81cfc2c538c3f2a836b42
DPS <smith.dp@gmail.com>
2019-10-06 20:24:10 +0100

    Constants - scalar, arrays and objects

---
+++ 2-const.js
@@ -0,0 +1,10 @@
+//Scalar values - defacto immutable
+const answer = 42;
+const greeting = 'Hello';
+
+//Arrays and Objects - the properties can still be changed, however the references will point at the same array or object, even if the constant changes
+const numbers = [2, 4, 6];
+const person = {
+    firstName: 'David',
+    lastName: 'Smith'
+};
