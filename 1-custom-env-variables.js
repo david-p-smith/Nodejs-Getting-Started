26d367bfaf863ce2a9cd9531fc900f7ebf306386
DPS <smith.dp@gmail.com>
2019-10-06 19:58:00 +0100

    Added node path

--- 1-custom-env-variables.js
+++ 1-custom-env-variables.js
@@ -4,3 +4,5 @@ console.log('\nScript executed with:');
 
 console.log('VAL1 equal to: ', process.env.VAL1);
 console.log('VAL2 equal to: ', process.env.VAL2);
+
+console.log('Node Path: ', process.env.NODE_PATH); 
