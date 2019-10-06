881d811a1f3354aff90ab9428c21aa139bc9e1d3
DPS <smith.dp@gmail.com>
2019-10-06 20:32:32 +0100

    new std func. X and arrow func. Y

--- 3-arrow-funcs.js
+++ 3-arrow-funcs.js
@@ -3,3 +3,13 @@ const square = (a) => {
 };
 
 console.log(square(2));
+
+const X = function () {
+    // "this" here is the caller of X
+};
+
+const Y = () => {
+    //"this" here is NOT the caller of Y
+
+    //It's actually the same "this" found in Y's scope
+};
