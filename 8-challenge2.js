3cb33f7576164d9e82d32dc16d6d2a7d487ee142
DPS <smith.dp@gmail.com>
2019-10-06 13:27:28 +0100

    Challenge 2  - prop. soln

--- 8-challenge2.js
+++ 8-challenge2.js
@@ -3,3 +3,19 @@
 // And stop after 5 times
 
 // After 5 times.  Print "Done" and let Node exit
+
+const message = 'Hello World';
+let counter = 0;
+
+const interval = setInterval(
+    () => {
+        console.log(message);
+        counter++;
+
+        if (counter === 5) {
+            clearInterval(interval);
+            console.log('Done');
+        }
+    },
+    1000
+);
