2fb87eedc5faa3a347a9a301259a0327e42609d0
DPS <smith.dp@gmail.com>
2019-10-07 19:58:53 +0100

    added circle object with func. param destructuring

--- 8-destructuring.js
+++ 8-destructuring.js
@@ -8,3 +8,12 @@ console.log(PI, E, SQRT2);
 
 //With require
 //const { readFile } = require('fs'); //read file now available using readFile() rather than fs.readFile()
+
+const circle = {
+    label: 'circleX',
+    radius: 2,
+};
+
+const circleArea = ({ radius }) => (PI * radius * radius).toFixed(2);
+
+console.log(cricleArea(circle));
