5abf2855830e1ebd8f8f950ce6517a385d3cb3cd
DPS <smith.dp@gmail.com>
2019-10-07 20:08:09 +0100

    rest spread on object

--- 9-rest-spread.js
+++ 9-rest-spread.js
@@ -2,3 +2,17 @@
 const [first, ...restOfItems] = [10, 20, 30, 40];
 
 console.log(first, restOfItems);
+
+const data = {
+    temp1: '001',
+    temp2: '002',
+    firstName: 'David',
+    lastName: 'Smith'
+};
+
+console.log(data);
+
+//object reset spread
+const { temp1, temp2, ...person } = data;
+
+console.log(temp1, temp2, person);
