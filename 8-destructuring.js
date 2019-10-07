47a3b83aedf7b59211b950b9ee58ff5f4de5e69b
DPS <smith.dp@gmail.com>
2019-10-07 18:37:58 +0100

    destructuring read file

--- 8-destructuring.js
+++ 8-destructuring.js
@@ -5,3 +5,6 @@
 const { PI, E, SQRT2 } = Math;
 
 console.log(PI, E, SQRT2);
+
+//With require
+//const { readFile } = require('fs'); //read file now available using readFile() rather than fs.readFile()
