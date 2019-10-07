d9b03be1568c6488708e2e258ad0fadc933c3917
DPS <smith.dp@gmail.com>
2019-10-07 18:30:52 +0100

    inline commenting and dynamic property to obj

--- 7-objects.js
+++ 7-objects.js
@@ -1,6 +1,9 @@
+const mystery = "meaningOfLife";
+
 const obj = {
     p1: 10,
     p2: 20,
-    f1() { },
-    f2: () => { },
+    f1() { }, //shorter object literal syntax
+    f2: () => { }, // arrow function
+    [mystery]: 42, //dynamic property name
 }
