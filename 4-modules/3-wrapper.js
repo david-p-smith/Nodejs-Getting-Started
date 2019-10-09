0e3ba4ce2479a136ff6de9683548b257b212b6c8
DPS <smith.dp@gmail.com>
2019-10-09 20:17:15 +0100

    wrapper var demo added

--- 4-modules/3-wrapper.js
+++ 4-modules/3-wrapper.js
@@ -1,4 +1,7 @@
 // function(exports, module, require, __filename, +__dirname) {
+
+let g = 1; //this is local in node - it's inside the wrapping function (unlike browsers)
+
 console.log(arguments);
 
 exports.a = 42; //exports is an argument
