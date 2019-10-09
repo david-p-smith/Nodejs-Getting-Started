dd484bd4a24e91168f59f982a2af21c1cea0fdb9
DPS <smith.dp@gmail.com>
2019-10-09 20:35:24 +0100

    node module function api

---
+++ 4-modules/2-examples/4-function.js
@@ -0,0 +1,12 @@
+//Top-level API is a function
+
+module.exports = title => `
+<html>
+    <head>
+        <title>${title}</title>
+    </head>
+    <body>
+        <h1>This is Node!</h1>
+    </body>
+</html>
+`;
