f2dd57a93f94152e70d2dd3ccfb4407006d80a77
DPS <smith.dp@gmail.com>
2019-10-09 20:57:24 +0100

    add invalid encoding - error inaccurate demo

--- 5-errors/2-try.js
+++ 5-errors/2-try.js
@@ -6,7 +6,7 @@ const files = ['1-loop.js', 'dfdfs', '2-test-data.txt'];
 files.forEach(file => {
     try {
         const filePath = path.resolve('./', file);
-        const data = fs.readFileSync(filePath);
+        const data = fs.readFileSync(filePath, 'utf-42');
         console.log('File data is', data);
     } catch (err) {
         console.log('File not found');
