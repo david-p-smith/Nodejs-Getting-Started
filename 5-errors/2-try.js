6c9b562182a3316b4258397d43eea81e67ddb67b
DPS <smith.dp@gmail.com>
2019-10-09 20:55:12 +0100

    making file read error a file not found exception

---
+++ 5-errors/2-try.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
+const files = ['1-loop.js', 'dfdfs', '2-test-data.txt'];
+
+files.forEach(file => {
+    try {
+        const filePath = path.resolve('./', file);
+        const data = fs.readFileSync(filePath);
+        console.log('File data is', data);
+    } catch (err) {
+        console.log('File not found');
+    }
+});
