1353746393a9a153348e39381de9bd97476e86fc
DPS <smith.dp@gmail.com>
2019-10-09 20:34:10 +0100

    made string neater, correct spacing

--- 4-modules/2-examples/3-string.js
+++ 4-modules/2-examples/3-string.js
@@ -1,10 +1,11 @@
 //Top-level API is a string
 
-module.exports = `<html>
-                    <head>
-                        <title>Hello World!</title>
-                    </head>
-                    <body>
-                        <h1>This is Node!</h1>
-                    </body>
-                </html>`;
+module.exports = `
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>This is Node!</h1>
+    </body>
+</html>`;
