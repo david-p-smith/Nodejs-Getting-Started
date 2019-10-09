e00aec91a4e5ce4cb620e80c5d688dc1ddbe6bc5
DPS <smith.dp@gmail.com>
2019-10-09 20:19:59 +0100

    module.exports api demo added

--- 4-modules/3-wrapper.js
+++ 4-modules/3-wrapper.js
@@ -5,6 +5,7 @@ let g = 1; //this is local in node - it's inside the wrapping function (unlike b
 console.log(arguments);
 
 exports.a = 42; //exports is an argument.  this is also an alias for module.exports
+module.exports.b = 37; //this is part of the same structure above - forming part of the api
 
 // return module.exports;
 // } (module.exports,...)
