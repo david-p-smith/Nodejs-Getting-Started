acd0726c65c3cd12669196726eefc49f5f583fe1
DPS <smith.dp@gmail.com>
2019-10-10 20:00:41 +0100

    server request handler rewritten using on

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -4,7 +4,10 @@ const requestListener = (req, res) => {
     res.end('Hello World.  This is David!\n');
 };
 
-const server = http.createServer(requestListener); //no ()!  This would use return value rather than function!
+//const server = http.createServer(requestListener); //no ()!  This would use return value rather than function!
+
+const server = http.createServer();
+server.on('request', requestListener);
 
 server.listen(4242, () => {
     console.log('Server is running...');
