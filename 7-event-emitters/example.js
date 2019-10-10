78e5204dd9327fdfa48ebf3aff28e2c5f5e4398e
DPS <smith.dp@gmail.com>
2019-10-10 19:51:50 +0100

    use setImmediate so emit is handled

--- 7-event-emitters/example.js
+++ 7-event-emitters/example.js
@@ -5,16 +5,16 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-myEmitter.emit('TEST_EVENT');
-
-myEmitter.on('TEST_EVENT', () => {
-    console.log('TEST_EVENT was fired');
-});
-myEmitter.on('TEST_EVENT', () => {
-    console.log('TEST_EVENT was fired');
-});
-myEmitter.on('TEST_EVENT', () => {
-    console.log('TEST_EVENT was fired');
+setImmediate(() => {
+    myEmitter.emit('TEST_EVENT');
 });
 
-myEmitter.emit('TEST_EVENT');
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
