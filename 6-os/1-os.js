4d3010b5b0bc007ceafaf60d90a8db83941535e3
DPS <smith.dp@gmail.com>
2019-10-11 21:37:53 +0100

    display number of cpu cores

--- 6-os/1-os.js
+++ 6-os/1-os.js
@@ -3,3 +3,5 @@ const os = require('os');
 console.log('OS platform: ', os.platform());
 
 console.log('OS CPU architecture: ', os.arch());
+
+console.log('# of logical CPU cores ', os.cpus().length);
