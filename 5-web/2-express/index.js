4f0dad14bd924eaf4851019950d2f92b5c16de15
DPS <smith.dp@gmail.com>
2019-10-11 21:24:53 +0100

    Server listener for root ('/')

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send('Hello Express!  This is David!')
+});
+
 server.listen(4242, () => {
     console.log('Express Server is running...');
 });
