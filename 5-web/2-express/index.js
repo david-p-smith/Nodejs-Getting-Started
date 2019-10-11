955ebbc542dab01dbb0ff261865956e03461a3a4
DPS <smith.dp@gmail.com>
2019-10-11 21:26:03 +0100

    added about page listener

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -6,6 +6,10 @@ server.get('/', (req, res) => {
     res.send('Hello Express!  This is David!')
 });
 
+server.get('/about', (req, res) => {
+    res.send('Hello Express!  This is David on the About page!')
+});
+
 server.listen(4242, () => {
     console.log('Express Server is running...');
 });
